ec98bd508c4899a9c2e9f77588a591e320838618
carlos alberto <carlos.v.torres.2002@gmail.com>
2022-09-23 09:42:32 -0500

    ejemplos de datos primitivos float

---
+++ Seccion2_Variables/Primitivos/src/PrimitivosFloat.java
@@ -0,0 +1,14 @@
+public class PrimitivosFloat {
+    public static void main (String[] args){
+
+        float realFloat = 1.5e4f;//15000f;
+        System.out.println("realFloat = " + realFloat);
+        System.out.println("float corresponde en Byte a = " + Float.BYTES);
+        System.out.println("float corresponde en Bites a = " + Float.SIZE);
+        System.out.println("Máximo valor para float = " + Float.MAX_VALUE);
+        System.out.println("Mínimo valor para float = " + Float.MIN_VALUE);
+        System.out.println("-------------------------------------------------");
+
+
+    }
+}
